0ce47dfa1395b99aaa99cd0cbd658acc08ac36c5
Xelti99 <frodo990927@gmail.com>
2024-06-22 00:27:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,21 @@
 
 # Tworzenie wirtualnego środowiska
 python3 -m venv venv
+if [ $? -ne 0 ]; then
+  echo "Failed to create virtual environment"
+  exit 1
+fi
 
 # Aktywacja wirtualnego środowiska
 source venv/bin/activate
+if [ $? -ne 0 ]; then
+  echo "Failed to activate virtual environment"
+  exit 1
+fi
 
 # Instalacja zależności
 pip install -r requirements.txt
+if [ $? -ne 0 ]; then
+  echo "Failed to install dependencies"
+  exit 1
+fi
